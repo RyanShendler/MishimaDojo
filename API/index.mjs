12701baafc92883fd2353a81708b6e3157e0b65e
RyanShendler <rshendler@jahnelgroup.com>
2022-10-07 16:54:02 -0400

    added ability to create new characters

--- API/index.mjs
+++ API/index.mjs
@@ -2,15 +2,110 @@ import { Neo4jGraphQL } from "@neo4j/graphql";
 import { ApolloServer, gql } from "apollo-server";
 import neo4j from "neo4j-driver";
 
-//prettier-ignore
-const typeDefs = gql``
+const typeDefs = gql`
+  type Character {
+    charID: ID! @id
+    lastModified: DateTime! @timestamp
+    name: String!
+    imageURL: String!
+    summary: String!
+    strengths: [String!]!
+    weaknesses: [String!]!
+    moves: [Move!]! @relationship(type: "HAS_MOVE", direction: OUT)
+    stances: [Stance!]! @relationship(type: "HAS_STANCE", direction: OUT)
+    combos: [Combo!]! @relationship(type: "HAS_COMBO", direction: OUT)
+    tags: [CharacterTag!]!
+      @relationship(type: "HAS_CHARACTER_TAG", direction: IN)
+  }
+
+  type Move {
+    moveID: ID! @id
+    lastModified: DateTime! @timestamp
+    name: String!
+    input: String!
+    startup: Int!
+    onHit: Int!
+    onCH: Int!
+    onBlock: Int!
+    summary: String!
+    damageHit: Int!
+    damageCH: Int!
+    users: [Character!]! @relationship(type: "HAS_MOVE", direction: IN)
+    stances: [Stance!]! @relationship(type: "STANCE_MOVE", direction: IN)
+    combos: [Combo!]! @relationship(type: "USES_LAUNCHER", direction: IN)
+    tags: [MoveTag!]! @relationship(type: "HAS_MOVE_TAG", direction: IN)
+  }
+
+  type Combo {
+    comboID: ID! @id
+    lastModified: DateTime! @timestamp
+    input: String!
+    damage: Int!
+    users: [Character!]! @relationship(type: "HAS_COMBO", direction: IN)
+    launchers: [Move!]! @relationship(type: "USES_LAUNCHER", direction: OUT)
+    tags: [ComboTag!]! @relationship(type: "HAS_COMBO_TAG", direction: IN)
+  }
+
+  type Stance {
+    stanceID: ID! @id
+    lastModified: DateTime! @timestamp
+    name: String!
+    notation: String!
+    summary: String!
+    transitions: [String!]!
+    users: [Character!]! @relationship(type: "HAS_STANCE", direction: IN)
+    moves: [Move!]! @relationship(type: "STANCE_MOVE", direction: OUT)
+  }
+
+  type CharacterTag {
+    charTagID: ID! @id
+    lastModified: DateTime! @timestamp
+    tag: String!
+    value: String!
+    characters: [Character!]!
+      @relationship(type: "HAS_CHARACTER_TAG", direction: OUT)
+  }
+
+  type ComboTag {
+    comboTagID: ID! @id
+    lastModified: DateTime! @timestamp
+    tag: String!
+    value: String!
+    combos: [Combo!]! @relationship(type: "HAS_COMBO_TAG", direction: OUT)
+  }
+
+  type MoveTag {
+    moveTagID: ID! @id
+    lastModified: DateTime! @timestamp
+    tag: String!
+    value: String!
+    moves: [Move!]! @relationship(type: "HAS_MOVE_TAG", direction: OUT)
+  }
+
+  type Mutation {
+    mergeCharacter(
+      name: String!
+      imageURL: String!
+      summary: String!
+      strengths: [String!]!
+      weaknesses: [String!]!
+    ): Character
+      @cypher(
+        statement: """
+        MERGE (c:Character {name:$name})
+        ON CREATE SET c.name = $name, c.imageURL = $imageURL, c.summary = $summary, c.strengths = $strengths, c.weaknesses = $weaknesses
+        RETURN c
+        """
+      )
+  }
+`;
 
 const driver = neo4j.driver(
   "bolt://localhost:7687",
   neo4j.auth.basic("neo4j", "brandy2024")
 );
 
-const neoSchema = new Neo4jGraphQL(typeDefs, driver);
+const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
 
 await neoSchema.getSchema();
 
